afa9d5f2f547ede5d0cd9276f10db05b29e1ac40
Chris Wells Wood <Chris Wells Wood>
2017-07-15 18:08:41 +0100

    Moved almost all of the code out of main.

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,69 @@
 extern crate rand;
+extern crate piston;
+extern crate graphics;
+extern crate glutin_window;
+extern crate opengl_graphics;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use glutin_window::GlutinWindow as Window;
+use graphics::Transformed;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::*;
+use piston::input::*;
+use piston::window::WindowSettings;
 
-    #[test]
-    fn make_a_tile() {
-        let tile = gobs::Tile::new(100.0, 100.0, 50.0, colours::MAGENTA);
-        assert_eq!(gobs::Point {
-                       x: 100.0,
-                       y: 100.0,
-                   },
-                   tile.pos);
-        assert_eq!(tile.colour, colours::MAGENTA);
+pub struct GameManager {
+    gl: GlGraphics,
+    started: bool,
+    board: gobs::Board,
+}
+
+impl GameManager {
+    fn render(&mut self, args: &RenderArgs) {
+        let board = &self.board;
+        self.gl.draw(args.viewport(), |c, gl| {
+            graphics::clear(colours::BLUE, gl);
+            for otile in board.tiles.iter() {
+                if otile.is_some() {
+                    let tile = otile.unwrap();
+                    let transform = c.transform
+                        .trans(tile.pos.x, tile.pos.y)
+                        .trans(-(tile.rect[2] / 2.0), -(tile.rect[3] / 2.0));
+                    graphics::rectangle(tile.colour, tile.rect, transform, gl);
+                }
+            }
+        });
+    }
+}
+
+pub fn run() {
+    const WINDOW_XY: f64 = 300.0;
+    let mut window: Window = WindowSettings::new("WHACK!", [WINDOW_XY as u32, WINDOW_XY as u32])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut game = GameManager {
+        gl: GlGraphics::new(OpenGL::V3_2),
+        started: false,
+        board: gobs::Board::from_length(WINDOW_XY),
+    };
+
+    println!("PRESS SPACE TO START!");
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            game.render(&r);
+        }
+
+        if let Some(Button::Keyboard(key)) = e.press_args() {
+            if key == Key::Space {
+                if !game.started {
+                    println!("START!");
+                    game::add_tile(&mut game.board);
+                }
+            }
+        }
     }
 }
 
