921f8115ed90fe75802227fc5251455cadf07e4e
Chris Wells Wood <cwwoodesq@gmail.com>
2017-07-30 22:49:55 +0100

    Moved modules into their own folders.

---
+++ src/colours.rs
@@ -0,0 +1,11 @@
+//! Defines constant values for various colours.
+
+pub type Colour = [f32; 4];
+pub const BLUE: Colour = [0.0, 0.0, 1.0, 1.0];
+pub const RED: Colour = [1.0, 0.0, 0.0, 1.0];
+pub const GREEN: Colour = [0.0, 1.0, 0.0, 1.0];
+pub const YELLOW: Colour = [1.0, 1.0, 0.0, 1.0];
+pub const MAGENTA: Colour = [1.0, 0.0, 1.0, 1.0];
+pub const CYAN: Colour = [0.0, 1.0, 1.0, 1.0];
+pub const WHITE: Colour = [1.0, 1.0, 1.0, 1.0];
+pub const BLACK: Colour = [0.0, 0.0, 0.0, 1.0];
